630e6cba1979d0cd326a5fafaa4bce6341b2c40e
ayamohammed910 <aya@ayas-MacBook-Air.local>
2025-08-07 00:49:33 +0300

    Counter_bloc

--- lib/main.dart
+++ lib/main.dart
@@ -5,92 +5,87 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(CounterApp());
-
 }
-class CounterApp extends StatelessWidget {
 
+class CounterApp extends StatelessWidget {
   const CounterApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     print("build");
     return BlocProvider(
-      
       //bloc
-      create: (BuildContext context)=> CounterBloc(),
+      create: (BuildContext context) => CounterBloc(),
 
       //cubit
-     // create: (BuildContext context)=> CounterCubit(),
+      // create: (BuildContext context)=> CounterCubit(),
       child: MaterialApp(
-      home: Scaffold(
-      body:Column(
-      children: [
-        //bloc
-          BlocBuilder<CounterBloc,CounterState>(
+        home: Scaffold(
+          body: Column(
+            children: [
+              //bloc
+              BlocBuilder<CounterBloc, CounterState>(
 //cubit
-        //  BlocBuilder<CounterCubit,CounterState>(
-      builder: (BuildContext context, state) {
-      print("build child");
+                  //  BlocBuilder<CounterCubit,CounterState>(
+                  builder: (BuildContext context, state) {
+                print("build child");
 
-      return Text(state.count.toString(),style: TextStyle(
-      fontSize: 30
-      ),);
-      }
+                return Text(
+                  state.count.toString(),
+                  style: TextStyle(fontSize: 30),
+                );
+              }),
+              //bloc
+              BlocBuilder<CounterBloc, CounterState>(
+                  //cubit
+                  // BlocBuilder<CounterCubit,int>(
+                  builder: (BuildContext context, state) {
+                print("build inc");
+
+                return TextButton(
+                  onPressed: () {
+                    context.read<CounterBloc>().add(IncreamentEvent());
+
+                  },
+                  child: Container(
+                    color: Colors.black,
+                    height: 50,
+                    width: 100,
+                    child: Center(
+                      child: const Text(
+                        '+',
+                        style: TextStyle(color: Colors.white, fontSize: 30.0),
+                      ),
+                    ),
+                  ),
+                );
+              }),
+              BlocBuilder<CounterBloc, CounterState>(
+                  builder: (BuildContext context, state) {
+                print("build inc");
+
+                return TextButton(
+                  onPressed: () {
+                    context.read<CounterBloc>().add(DecreamentEvent());
+                  },
+                  child: Container(
+                    color: Colors.black,
+                    height: 50,
+                    width: 100,
+                    child: Center(
+                      child: const Text(
+                        '-',
+                        style: TextStyle(color: Colors.white, fontSize: 30.0),
+                      ),
+                    ),
+                  ),
+                );
+              })
+            ],
+          ),
+        ),
       ),
-      //bloc
-      BlocBuilder<CounterBloc,CounterState>(
-      //cubit
-     // BlocBuilder<CounterCubit,int>(
-      builder: (BuildContext context, state) {
-      print("build inc");
-
-      return TextButton(
-      onPressed: () {
-      context.read<CounterBloc>().add(IncreamentEvent());
-      },
-      child: Container(
-      color: Colors.black,
-      height: 50,
-      width: 100,
-      child: Center(
-      child: const Text(
-      '+',
-      style: TextStyle(color: Colors.white, fontSize: 30.0),
-
-      ),
-      ),
-      ),
-      )
-      ;
-      }),
-    BlocBuilder<CounterBloc,CounterState>(
-    builder: (BuildContext context, state) {
-    print("build inc");
-
-    return TextButton(
-      onPressed: () {
-      context.read<CounterBloc>().add(DecreamentEvent());
-
-      },
-      child: Container(
-      color: Colors.black,
-    height: 50,
-    width: 100,
-    child: Center(
-    child: const Text(
-    '-',
-    style: TextStyle(color: Colors.white, fontSize: 30.0),
-
-    ),
-    ),
-    ),
-    );})
-
-    ],
-    ),
-    ),
-    )
-      ,
     );
   }
 }
+
